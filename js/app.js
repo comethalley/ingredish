c0e3aec86c2af8b084e5400ac733c14e07c48833
comethalley <83149734+comethalley@users.noreply.github.com>
2023-07-26 11:10:53 +0800

    card design

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const apiUrl = "https://api.spoonacular.com";
+const endpoint = "recipes/findByIngredients";
+const apiKey = "27b78a980d26472eb17d8ee92d95dc71";
+
 document.getElementById("submit").onclick = function () {
   const query = document.getElementById("search").value;
   const ingredients = query.split(",").map((ingredient) => ingredient.trim());
@@ -6,7 +10,7 @@ document.getElementById("submit").onclick = function () {
   const ingredientsString = ingredients.join(",+");
 
   fetch(
-    `https://api.spoonacular.com/recipes/findByIngredients?apiKey=27b78a980d26472eb17d8ee92d95dc71&ingredients=${query}`
+    `${apiUrl}/${endpoint}?apiKey=${apiKey}&ingredients=${ingredientsString}`
   )
     .then((res) => {
       console.log(res);
@@ -22,10 +26,10 @@ document.getElementById("submit").onclick = function () {
         list += "</p>";
 
         const markup = `<div class="recipe-card">
-        <div class="card-image" ><img src="${recipe.image}" class="recipe-img"/></div>
+        <img src="${recipe.image}" class="recipe-img"/>
         <div class="card-title"><h3>${recipe.title}</h3></div>
-        <div class="card-info"><h4>Ingredients</h4>${list}</div>
-        <a href="recipe.html/${recipe.id}">See the recipe</a>
+        <div class="card-info"><h5>Ingredients</h5>${list}</div>
+        <a href="recipe.html?${recipe.id}">See the recipe</a>
       </div>`;
 
         document
